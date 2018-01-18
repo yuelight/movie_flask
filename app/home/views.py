d0210f09bac9872600c5c1ac663dbc39887fcf7e
yuelight <vaeqfe@gmail.com>
2018-01-18 16:53:33 +0800

    add static html files

--- app/home/views.py
+++ app/home/views.py
@@ -17,3 +17,35 @@ def logout():
 @home.route('/register/')
 def register():
     return render_template('home/register.html')
+
+@home.route('/user/')
+def user():
+    return render_template('home/user.html')
+
+@home.route('/pwd/')
+def pwd():
+    return render_template('home/pwd.html')
+
+@home.route('/comments/')
+def comments():
+    return render_template('home/comments.html')
+
+@home.route('/loginlog/')
+def loginlog():
+    return render_template('home/loginlog.html')
+
+@home.route('/moviecol/')
+def moviecol():
+    return render_template('home/moviecol.html')
+
+@home.route('/animation/')
+def animation():
+    return render_template('home/animation.html')
+
+@home.route('/search/')
+def search():
+    return render_template('home/search.html')
+
+@home.route('/play/')
+def play():
+    return render_template('home/play.html')
